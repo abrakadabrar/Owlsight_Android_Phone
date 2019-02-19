4cfde5a2c7cce949d8b75e9e5931c3c776228911
abrakadabrar <neoname0q@gmail.com>
2019-02-19 18:50:48 +0300

    added getUpdate thumb and fix isRechable

---
+++ app/src/main/java/com/cryptocenter/andrey/owlsight/ui/screens/dark_dialog/DarkDialogFragment.java
@@ -0,0 +1,6 @@
+package com.cryptocenter.andrey.owlsight.ui.screens.dark_dialog;
+
+import com.cryptocenter.andrey.owlsight.base.BaseFragment;
+
+public class DarkDialogFragment extends BaseFragment {
+}
---
+++ app/src/main/java/com/cryptocenter/andrey/owlsight/ui/screens/dark_dialog/DarkDialogView.java
@@ -0,0 +1,4 @@
+package com.cryptocenter.andrey.owlsight.ui.screens.dark_dialog;
+
+public interface DarkDialogView {
+}
