97d4bc85d95d6cd30d93c01271f934ebc1e76c2f
abrakadabrar <neoname0q@gmail.com>
2019-02-22 11:02:50 +0300

    Update AddCameraActivity.java
    some fix

--- app/src/main/java/com/cryptocenter/andrey/owlsight/ui/screens/add_camera/AddCameraActivity.java
+++ app/src/main/java/com/cryptocenter/andrey/owlsight/ui/screens/add_camera/AddCameraActivity.java
@@ -30,39 +30,7 @@ public class AddCameraActivity extends BaseActivity implements AddCameraView {
 
     @InjectPresenter
     AddCameraPresenter presenter;
-    @BindView(R.id.btnClose)
-    ImageButton btnClose;
-    @BindView(R.id.toolbar)
-    Toolbar toolbar;
-    @BindView(R.id.etCamName)
-    TextInputEditText etCamName;
-    @BindView(R.id.tvErrorCamName)
-    TextView tvErrorCamName;
-    @BindView(R.id.etCamHost)
-    TextInputEditText etCamHost;
-    @BindView(R.id.tvErrorCamHost)
-    TextView tvErrorCamHost;
-    @BindView(R.id.etCamPort)
-    TextInputEditText etCamPort;
-    @BindView(R.id.tvErrorCamPort)
-    TextView tvErrorCamPort;
-    @BindView(R.id.etCamExtra)
-    TextInputEditText etCamExtra;
-    @BindView(R.id.tvErrorCamOptions)
-    TextView tvErrorCamOptions;
-    @BindView(R.id.etCamUser)
-    TextInputEditText etCamUser;
-    @BindView(R.id.tvErrorCamUser)
-    TextView tvErrorCamUser;
-    @BindView(R.id.tvErrorCamPass)
-    TextView tvErrorCamPass;
-    @BindView(R.id.textViewdata)
-    TextView textViewdata;
-    @BindView(R.id.etCamPassword)
-    TextView etPassword;
-    @BindView(R.id.btnCamNext)
-    Button btnCamNext;
-
+   
 
     public static Intent intent(Context context, int cameraId) {
         final Intent intent = new Intent(context, AddCameraActivity.class);
@@ -81,17 +49,6 @@ public class AddCameraActivity extends BaseActivity implements AddCameraView {
         getWindow().setFlags(FLAG_FULLSCREEN, FLAG_FULLSCREEN);
         setContentView(R.layout.activity_add_camera);
         ButterKnife.bind(this);
-    }
-
-    @OnClick(R.id.btnCamNext)
-    public void onViewClicked() {
-        presenter.addCamera(etCamName.getText().toString(),
-                etCamHost.getText().toString(),
-                etCamPort.getText().toString(),
-                etCamExtra.getText().toString(),
-                etCamUser.getText().toString(),
-                etCamUser.getText().toString(),
-                etPassword.getText().toString());
     }
 
     // =============================================================================================
