cfd5ebf60ee1c1bdb26b27651a16896f867fb2b7
danilashamin <danilashamin@gmail.com>
2019-03-02 14:43:23 +0300

    add crashlytics

--- app/src/main/java/com/cryptocenter/andrey/owlsight/App.java
+++ app/src/main/java/com/cryptocenter/andrey/owlsight/App.java
@@ -2,9 +2,12 @@ package com.cryptocenter.andrey.owlsight;
 
 import android.app.Application;
 
+import com.crashlytics.android.Crashlytics;
+import com.crashlytics.android.core.CrashlyticsCore;
 import com.cryptocenter.andrey.owlsight.di.DependencyInjection;
 
 import es.dmoral.toasty.Toasty;
+import io.fabric.sdk.android.Fabric;
 
 public class App extends Application {
 
@@ -13,5 +16,24 @@ public class App extends Application {
         super.onCreate();
         Toasty.Config.getInstance().apply();
         DependencyInjection.configure(this);
+        initCrashlytics();
+    }
+
+    private void initCrashlytics() {
+        CrashlyticsCore core = new CrashlyticsCore.Builder()
+//                .disabled(BuildConfig.DEBUG)
+                .build();
+
+        Crashlytics crashlytics = new Crashlytics.Builder()
+                .core(core)
+                .build();
+
+        Fabric fabric = new Fabric
+                .Builder(this)
+                .debuggable(true)
+                .kits(crashlytics).build();
+
+        Fabric.with(fabric);
+
     }
 }
