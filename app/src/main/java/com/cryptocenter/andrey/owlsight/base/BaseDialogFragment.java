493c6ca598045e95ab4706534ae8a90b238f45e6
danilashamin <danilashamin@gmail.com>
2019-04-09 12:15:24 +0300

    dialog fragment

--- app/src/main/java/com/cryptocenter/andrey/owlsight/base/BaseDialogFragment.java
+++ app/src/main/java/com/cryptocenter/andrey/owlsight/base/BaseDialogFragment.java
@@ -1,64 +1,130 @@
 package com.cryptocenter.andrey.owlsight.base;
 
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.app.DialogFragment;
-import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.cryptocenter.andrey.owlsight.App;
 import com.cryptocenter.andrey.owlsight.R;
+import com.cryptocenter.andrey.owlsight.base.moxy.MvpAndroidXDialogFragment;
+import com.cryptocenter.andrey.owlsight.data.model.data.FromDateData;
+import com.cryptocenter.andrey.owlsight.ui.screens.add_camera.AddCameraActivity;
+import com.cryptocenter.andrey.owlsight.ui.screens.choose_group.ChooseGroupActivity;
+import com.cryptocenter.andrey.owlsight.ui.screens.groups.GroupsActivity;
+import com.cryptocenter.andrey.owlsight.ui.screens.player.SinglePlayerActivity;
+import com.cryptocenter.andrey.owlsight.ui.screens.stream.StreamActivity;
+import com.cryptocenter.andrey.owlsight.ui.screens.video.FromDateActivity;
+import com.cryptocenter.andrey.owlsight.utils.Alerts;
+import com.cryptocenter.andrey.owlsight.utils.Screen;
+import com.kaopiz.kprogresshud.KProgressHUD;
 
-import butterknife.ButterKnife;
-import butterknife.Unbinder;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import es.dmoral.toasty.Toasty;
 
-public abstract class BaseDialogFragment extends androidx.fragment.app.DialogFragment {
+public abstract class BaseDialogFragment extends MvpAndroidXDialogFragment implements BaseView {
+    protected abstract int getLayoutResId();
+    private KProgressHUD loader;
+    @Override
+    public void bind() {
 
-    private Unbinder mUnbinder;
-
-    protected Activity mActivity;
+    }
 
     @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof BaseActivity) {
-            mActivity = (Activity) context;
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(getLayoutResId(), container, false);
+    }
+
+    @Override
+    public void showLoading() {
+        loader = Alerts.loader(getActivity());
+        loader.show();
+    }
+
+    @Override
+    public void hideLoading() {
+        if (loader != null) {
+            loader.dismiss();
+            loader = null;
         }
     }
 
     @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
-        if (activity instanceof BaseActivity) {
-            mActivity = activity;
+    public void showFailed() {
+        Toasty.error(getActivity(), App.getInstance().getString(R.string.something_went_wrong), Toast.LENGTH_SHORT, true).show();
+    }
+
+    @Override
+    public void showError(Throwable error) {
+        Toasty.error(getActivity(), App.getInstance().getString(R.string.network_error), Toast.LENGTH_SHORT, true).show();
+    }
+
+    public void addScreen(Screen screen, Object data) {
+        switch (screen) {
+            case SINGLE_PLAYER:
+                startActivity(SinglePlayerActivity.intent(getActivity(), (String) data));
+                break;
+            case VIDEO_FROM_DATE:
+                startActivity(FromDateActivity.intent(getActivity(), (FromDateData) data));
+                break;
+            case ADD_CAMERA:
+                startActivity(AddCameraActivity.intent(getActivity(), (int) Integer.parseInt((String) data)));
+                break;
+            case STREAM:
+                StreamActivity.start(getActivity());
+                break;
+            case CHOOSE_GROUP:
+                ChooseGroupActivity.start(getActivity());
+                break;
         }
     }
 
     @Override
-    public Dialog onCreateDialog(Bundle savedInstanceState) {
-        LayoutInflater inflater = getActivity().getLayoutInflater();
-        View view = inflater.inflate(getLayout(), null, false);
-        mUnbinder = ButterKnife.bind(this, view);
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setView(view);
-        return builder.create();
+    public void addScreenForResult(Screen screen, int requestCode, Object... data) {
+
     }
 
-    protected abstract int getLayout();
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        if (mUnbinder != null) {
-            mUnbinder.unbind();
+    public void showScreen(Screen screen) {
+        if (loader.isShowing()) hideLoading();
+        getActivity().finish();
+        switch (screen) {
+            case GROUPS:
+                getActivity().startActivity(GroupsActivity.intent(getActivity()));
+                break;
         }
     }
 
+    public void closeScreen(@Nullable String message) {
+        if (message != null) showMessage(message);
+        getActivity().finish();
+    }
+
     @Override
-    public void onDestroy() {
-        super.onDestroy();
-        mActivity = null;
+    public void closeScreenWithResult(int resultCode, String message) {
+
+    }
+
+    public void showMessage(String message) {
+        Toast.makeText(getActivity(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void showAlertMessage(String title, String message) {
+        new AlertDialog.Builder(getActivity()).setTitle(title).setMessage(message).setPositiveButton("OK", (dialogInterface, i) -> {
+        }).show();
+    }
+
+    @Override
+    public void onBackClicked() {
+
     }
 }
